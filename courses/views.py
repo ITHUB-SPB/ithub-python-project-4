d715fabcac9beb199078dba0a56d52d4dd5436eb
darya <qq1fsd@gmail.com>
2026-05-27 20:53:22 +0300

    url'ки и вьюшки сделаны юху

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from courses import models
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
 # Create your views here.
+@login_required(login_url=reverse_lazy('login'))
+def index(request):
+    group = request.user.student.group
+    courses = models.Course.objects.filter(group=group)
+    return render(request, 'courses.html', { 'courses': courses })
+
+@login_required(login_url=reverse_lazy('login'))
+def detail(request, course_id):
+    course = models.Course.objects.get(pk=course_id)
+    return render(request, 'course_detail.html', { 'course': course })
--- students/views.py
+++ students/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import auth, messages
+from django.urls import reverse
 
 # Create your views here.
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = auth.authenticate(request, username=username, password=password)
+
+    if not user:
+        messages.error(request, 'Не удалось войти')
+        return redirect(reverse('login'))
+        
+    auth.login(request, user)
+    return redirect(reverse('courses'))
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('login'))
