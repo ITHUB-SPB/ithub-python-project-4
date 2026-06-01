b451719a41d8d83970d45dd9307c86d70c0695c5
darya <qq1fsd@gmail.com>
2026-06-02 00:45:17 +0300

    алексей намудрил

--- students/admin.py
+++ students/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from unfold.admin import ModelAdmin
 from students.models import Group, Student
 
-class GroupAdmin(admin.ModelAdmin):
+class GroupAdmin(ModelAdmin):
     list_display = ['title', 'year']
     list_filter = ['year']
     search_fields = ['title']
@@ -10,7 +10,7 @@ class GroupAdmin(admin.ModelAdmin):
 admin.site.register(Group, GroupAdmin)
 
 # Register your models here.
-class StudentAdmin(admin.ModelAdmin):
+class StudentAdmin(ModelAdmin):
     list_display = ('first_name', 'last_name', 'middle_name', 'account', 'group')
     search_fields = ('last_name', 'account__username', 'account__email')
 
